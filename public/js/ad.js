d42b1a6fb5d48777f81977ff1b3fb80745a9d685
greg <gregkimovec@gmail.com>
2019-07-12 11:59:45 +0200

    Gestion des annonces, formulaire de creation et edition

---
+++ public/js/ad.js
@@ -0,0 +1,24 @@
+$('#add-image').click(function(){
+    //je recupère le numéro des futurs champs que je vais créer
+    const index = +$('#widgets-counter').val();
+    
+    //je recupère le prototype des entrées
+    const tmpl = $('#annonce_images').data('prototype').replace(/__name__/g, index);
+
+    //j'injecte ce code au sein de la div
+    $('#annonce_images').append(tmpl);
+
+    $('#widgets-counter').val(index + 1);
+
+    //boutton de suppression
+    handleDeleteButtons();
+});
+
+function handleDeleteButtons(){
+    $('button[data-action="delete"]').click(function(){
+        const target = this.dataset.target;
+        $(target).remove();
+    });
+};
+
+handleDeleteButtons();
